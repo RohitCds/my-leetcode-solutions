26a8977cfe209a59ec2ea7b1c45add1b0055e956
RohitCds <114694777+RohitCds@users.noreply.github.com>
2023-10-25 17:40:51 +0000

    Sync LeetCode submission Runtime - 12 ms (17.50%), Memory - 13.8 MB (100.00%)

---
+++ my-solutions/0073-set-matrix-zeroes/solution.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool row0 = false, col0 = false;
+
+        // Check if first row and first column should be set to 0s
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+
+        // Use first row and first column to store information
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Modify matrix based on first row and first column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Modify first row and first column if necessary
+        if (col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (row0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
+
