eb28268c114bfa30198ef0bc320b9d5eb6df50e4
RohitCds <114694777+RohitCds@users.noreply.github.com>
2023-11-10 20:18:02 +0000

    Sync LeetCode submission Runtime - 3 ms (1.76%), Memory - 11.7 MB (100.00%)

---
+++ my-solutions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    // Target is in the left half
+                    right = mid - 1;
+                } else {
+                    // Target is in the right half
+                    left = mid + 1;
+                }
+            } else {
+                // The right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    // Target is in the right half
+                    left = mid + 1;
+                } else {
+                    // Target is in the left half
+                    right = mid - 1;
+                }
+            }
+        }
+
+        // Target not found
+        return -1;
+    }
+};
+
