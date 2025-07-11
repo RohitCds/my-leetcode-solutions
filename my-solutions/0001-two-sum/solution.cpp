4fdfc838b9aaaee6b7533b379bec09a854d159de
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-07-11 19:22:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.7 MB (54.49%)

--- my-solutions/0001-two-sum/solution.cpp
+++ my-solutions/0001-two-sum/solution.cpp
@@ -1,18 +1,23 @@
+// File: solution.cpp
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hashMap;  // key: number, value: index
-        vector<int> result;
+        unordered_map<int, int> num_to_index;
+        
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
-            if (hashMap.find(complement) != hashMap.end()) {
-                result.push_back(hashMap[complement]);
-                result.push_back(i);
-                return result;
+            if (num_to_index.count(complement)) {
+                return {num_to_index[complement], i};
             }
-            hashMap[nums[i]] = i;
+            num_to_index[nums[i]] = i;
         }
-        return result;  // return an empty vector if no solution is found
+
+        return {};  // unreachable due to problem constraints
     }
 };
 
