d82e5c356cbe16415e602f8907a98e2bec87ca9f
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-07-13 18:39:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 77.2 MB (44.38%)

--- my-solutions/0002-add-two-numbers/solution.cpp
+++ my-solutions/0002-add-two-numbers/solution.cpp
@@ -15,23 +15,22 @@ public:
         ListNode* current = &dummy;
         int carry = 0;
 
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+        while(l1 || l2|| carry)
+        {
             int sum = carry;
-            if (l1 != nullptr) {
+            if(l1){
                 sum += l1->val;
                 l1 = l1->next;
             }
-            if (l2 != nullptr) {
-                sum += l2->val;
+            if(l2){
+                sum+= l2->val;
                 l2 = l2->next;
             }
 
-            carry = sum / 10;
-            current->next = new ListNode(sum % 10);
+            carry = sum/10;
+            current ->next = new ListNode(sum%10);
             current = current->next;
         }
-
         return dummy.next;
     }
 };
-
