8820ae7b84fb8f790109ed0cc5d4e0b8eaa3fa4a
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-07-16 19:06:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.3 MB (21.67%)

--- my-solutions/0008-string-to-integer-atoi/solution.cpp
+++ my-solutions/0008-string-to-integer-atoi/solution.cpp
@@ -1,29 +1,28 @@
-#include <iostream>
 #include <string>
-
+#include <climits>
 using namespace std;
+
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
-        int n = s.length();
+        int start = 0;
+        while(start<s.length() && s[start] == ' '){
+            start++;
+        }
         int sign = 1;
-        long long result = 0;
-        while(i<n&&s[i]==' ') i++;
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-        sign = (s[i] == '-') ? -1 : 1;
-        i++;
+        if (start<s.length() && (s[start]=='-' || s[start]=='+')) {
+            sign = (s[start]=='-')? -1:1;
+            start++;
         }
-        while (i < n && isdigit(s[i])) {
-        result = result * 10 + (s[i] - '0');
-        if (result * sign > INT_MAX) {
-            return INT_MAX;
-        }
-        if (result * sign < INT_MIN) {
-            return INT_MIN;
-        }
-        i++;
+        long result = 0;
+        while(start<s.length() && isdigit(s[start])){
+            result = result*10 + (s[start] - '0');
+            if (result*sign>INT_MAX) return INT_MAX;
+            if (result*sign<INT_MIN) return INT_MIN;
+            start++;
         }
         return result*sign;
     }
 };
+
+
