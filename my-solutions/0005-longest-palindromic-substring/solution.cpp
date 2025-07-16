fb5c118ea04b827e3917b04e5b2ac9bc196527ba
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-07-16 18:29:07 +0000

    Sync LeetCode submission Runtime - 11 ms (64.24%), Memory - 9.4 MB (64.07%)

--- my-solutions/0005-longest-palindromic-substring/solution.cpp
+++ my-solutions/0005-longest-palindromic-substring/solution.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        int maxlength = 0;
         int start = 0;
-        int maxlen = 1;
-        for(int i = 0; i< s.length(); i++){
-            int len1 = expansion(s,i,i);
-            int len2 = expansion(s, i, i+1);
-            int len = max(len1, len2);
-            if(maxlen<len){
-                maxlen = len;
-                start = i-((maxlen-1)/2);
-            }
+        for (int i =0; i<s.length(); i++){
+            expandfromcenter(s, i, i, start, maxlength);
+            expandfromcenter(s, i, i+1, start, maxlength);
         }
-        return s.substr(start, maxlen);
+        return s.substr(start, maxlength);
     }
-    int expansion(string str, int left, int right){
-        while((left>=0)&&(right<str.length())&&(str[left]==str[right])){
+
+private:
+    void expandfromcenter(const string& a, int left, int right, int& s, int& m){
+        while(left>=0 && right <a.length() && a[left]==a[right]){
+            if (right-left+1 > m){
+                m = right - left + 1;
+                s = left;
+            }
             left--;
             right++;
         }
-        return right-left-1;
     }
+
 };
