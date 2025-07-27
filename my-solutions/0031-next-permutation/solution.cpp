60b4d677b40ddb64f971e8c4c34d2d9eeab55dc3
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-07-27 18:30:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.5 MB (99.85%)

--- my-solutions/0031-next-permutation/solution.cpp
+++ my-solutions/0031-next-permutation/solution.cpp
@@ -1,28 +1,19 @@
-#include <vector>
-#include <algorithm>  // For the reverse function
-using namespace std;
-
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i = nums.size() - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+        int n = nums.size();
+        if(n<=1) return;
+        int i = n - 2;
+        while(i>=0 && nums[i]>= nums[i+1]){
             i--;
         }
-
-        if (i >= 0) {  // Check whether we found such 'i'
-            int m = nums.size() - 1;
-            while (m > i) {
-                if (nums[m] > nums[i]) {
-                    swap(nums[m], nums[i]);
-                    break;
-                }
-                m--;
+        if(i>=0){
+            int j = n-1;
+            while(nums[j]<=nums[i]){
+                j--;
             }
-            reverse(nums.begin() + i + 1, nums.end());
-        } else {  // This means the sequence is the last permutation
-            reverse(nums.begin(), nums.end());
+            swap(nums[i],nums[j]);
         }
+        reverse(nums.begin() + i +1, nums.end());
     }
 };
-
