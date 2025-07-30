850803b1bad4edc0fe7508b0f6466b2c3e125b6e
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-07-30 18:59:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (68.18%)

--- my-solutions/0048-rotate-image/solution.cpp
+++ my-solutions/0048-rotate-image/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        //the trick is to first transpose the matrix and then reverse its columns;
         int n = matrix.size();
-        int m = matrix[0].size();
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<m; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n/2; j++){
-                swap(matrix[i][j], matrix[i][m-j-1]);
+        for (int i = 0; i < n; i++) {
+            int low = 0, high = n - 1;
+            while (low < high) {
+                swap(matrix[i][low], matrix[i][high]);
+                low++;
+                high--;
             }
         }
     }
 };
+
