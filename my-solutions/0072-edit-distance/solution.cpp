5ce2edb807d436cf0aa87828cb68aa71c763ce73
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-08-06 13:09:12 +0000

    Sync LeetCode submission Runtime - 1 ms (95.36%), Memory - 10.3 MB (98.73%)

---
+++ my-solutions/0072-edit-distance/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        
+        vector<int> prev(m + 1), curr(m + 1);
+        
+        for(int j = 0; j <= m; j++){
+            prev[j] = j;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            curr[0] = i;  
+            
+            for(int j = 1; j <= m; j++){
+                if(word1[i-1] == word2[j-1]){
+                    curr[j] = prev[j-1];  
+                }
+                else{
+                    int replace = prev[j-1];
+                    int insert = curr[j-1];
+                    int delete_op = prev[j];
+                    
+                    curr[j] = min({replace, insert, delete_op}) + 1;
+                }
+            }
+            
+            prev = curr; 
+        }
+        
+        return prev[m];
+    }
+};
