b6ad3728568e6b95feb1021d1fd774451c917b2d
RohitCds <114694777+RohitCds@users.noreply.github.com>
2025-08-09 19:20:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (58.75%)

---
+++ my-solutions/0060-permutation-sequence/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> numbers;
+        int fact = 1;
+        for(int i = 1; i<n; i++){
+            fact=fact*i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(n);
+        k--;
+        string a = "";
+        while(true){
+            a+=to_string(numbers[k/fact]);
+            numbers.erase(numbers.begin()+k/fact);
+            if(numbers.size()==0) break;
+            k%=fact;
+            fact/=numbers.size();            
+        }  
+        return a;
+    }
+};
+
